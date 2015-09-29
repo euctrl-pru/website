66e3ed78ccec6621112d57ea4165c652e8a4613a
Enrico Spinielli <enrico.spinielli@gmail.com>
2015-09-29 16:13:44 +0200

    bar charts start from zero

--- _assets/javascripts/crt_flts_MM.js
+++ _assets/javascripts/crt_flts_MM.js
@@ -1,27 +1,70 @@
 (function() {
-   function dv(){
-// TRAFFIC - Controlled flights - MM - CHART
-   var crt_flts_MM = new google.visualization.ChartWrapper({
-      chartType: 'ComboChart',
-      containerId: 'crt_flts_MM',
-      dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1Ap6_pXhVLZcgfUcdbqsUBvZaI_AdVsT35XLY1oPKyJ4/edit?usp=sharing&sheet=ERT_FLTS_MM&range=A1:F25&headers=1&tq=where%20A%3D1',
-      options: {   
-            title: 'Avg. daily flights (monthly)', titleTextStyle : {color: 'grey', fontSize: 12},
-            chartArea: {left: 50, top: 50, width:'70%',height:'55%'},
-            legend: {position: 'top',  maxLines: 2, textStyle:{color: 'grey', fontSize: 10}},      
-            series: {
-                     0: {type: 'bars', color:'#B9CDE5', targetAxisIndex: 0},
-                     1: {type: 'lines', color:'#376092', lineWidth: 2, pointSize: 7, targetAxisIndex: 1},
-                2: {type: 'lines', color:'#777777', lineWidth: 3, lineDashStyle:[6,3], targetAxisIndex: 0}
+   function dv() {
+      // TRAFFIC - Controlled flights - MM - CHART
+      var crt_flts_MM = new google.visualization.ChartWrapper(
+         {
+            chartType: 'ComboChart',
+            containerId: 'crt_flts_MM',
+            dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1Ap6_pXhVLZcgfUcdbqsUBvZaI_AdVsT35XLY1oPKyJ4/edit?usp=sharing&sheet=ERT_FLTS_MM&range=A1:F25&headers=1&tq=where%20A%3D1',
+            options: {
+               title: 'Avg. daily flights (monthly)',
+               titleTextStyle : { color: 'grey', fontSize: 12 },
+               chartArea: { left: 50, top: 50, width: '70%', height: '55%' },
+               legend: {
+                  position: 'top',
+                  maxLines: 2,
+                  textStyle: { color: 'grey', fontSize: 10 }
+               },
+               series: {
+                  0: {
+                     type: 'lines',
+                     color:'#B9CDE5',
+                     lineWidth: 2,
+                     pointSize: 7,
+                     targetAxisIndex: 0
                   },
-            vAxes:{
-                     0:{ title:'Avg. daily flights', titleTextStyle : {fontSize: 11, color: 'grey',}, minValue: 20000 ,viewWindow: {min: 20000}, format: 'short'},
-                     1:{ title:'cum. change vs prev. year (%)',titleTextStyle : {color: '#376092', fontSize: 11}, textStyle:{color:'#376092'}}
+                  1: {
+                     type: 'lines',
+                     color:'#376092',
+                     lineWidth: 2,
+                     pointSize: 7,
+                     targetAxisIndex: 1
                   },
-            hAxis: {title: 'Source: PRU Analysis; NM', titleTextStyle: {color: 'grey', fontSize: 10}, type:'string', slantedTextAngle: 90, textStyle: {fontSize: 11}}
+                  2: {
+                     type: 'lines',
+                     color:'#777777',
+                     lineWidth: 3,
+                     pointSize: 7,
+                     lineDashStyle: [6, 3],
+                     targetAxisIndex: 0
+                  }
+               },
+               vAxes:{
+                  0: {
+                     title:'Avg. daily flights',
+                     titleTextStyle : { fontSize: 11, color: 'grey' },
+                     minValue: 20000,
+                     viewWindow: { min: 20000 },
+                     format: 'short'
+                  },
+                  1: {
+                     title:'cum. change vs prev. year (%)',
+                     titleTextStyle : { color: '#376092', fontSize: 11 },
+                     textStyle: { color:'#376092' }
+                  }
+               },
+               hAxis: {
+                  title: 'Source: PRU Analysis; NM',
+                  titleTextStyle: { color: 'grey', fontSize: 10 },
+                  type:'string',
+                  slantedTextAngle: 40,
+                  textStyle: { fontSize: 11 }
+               }
             },
-      view: {columns: [1,3,4,5]}
-   }); crt_flts_MM.draw();
+            view: { columns: [1, 3, 4, 5] }
+         });
+      crt_flts_MM.draw();
    }
-   google.setOnLoadCallback(dv)
+
+   google.setOnLoadCallback(dv);
 })();
--- _assets/javascripts/crt_hfe_MM.js
+++ _assets/javascripts/crt_hfe_MM.js
@@ -1,25 +1,70 @@
 (function() {
-   function dv(){   
-// ENVIRONMENT - Horizontal flight efficiency - MM - CHART
-var crt_hfe_MM = new google.visualization.ChartWrapper({
-      chartType: 'LineChart',
-      containerId: 'crt_hfe_MM',
-      dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1KgOIBF93axvXD6ENQYvEsWHqnA8eW_RvGV4EQlGava4/edit?usp=sharing&sheet=HFE_MM&range=A1:H25&tq=where%20A%3D1',
-      options:{   
-               chartArea: {left: 50, top: 20, width:'55%',height:'60%'},
-               title: 'Horiz. en-route flight eff. (monthly)', titleTextStyle : {color: 'grey', fontSize: 11},
-               legend: {position:'right', textStyle:{color: 'grey', fontSize: 10}},
-               series: {
-                     0: {type: 'lines', color:'#5D7CBA', lineWidth: 1},
-                     1: {type: 'lines', color:'#C0504D', lineWidth: 1},
-                     2: {type: 'lines', color:'#5D7CBA', lineWidth: 2, lineDashStyle:[6,3]},
-                     3: {type: 'lines', color:'#C0504D', lineWidth: 2, lineDashStyle:[6,3]}            
-                  },    
-               vAxis: {title: 'inefficiency (%)', titleTextStyle: {color: 'grey', fontSize: 10}, minValue: 0.0, maxValue: 0.06, viewWindow: {min: 0.02, max: 0.05}, format: '0.0%'},
-               hAxis: {title: 'Source: PRU Analysis', titleTextStyle: {color: 'grey', fontSize: 10}, type:'string', slantedTextAngle: 90, textStyle: {fontSize: 11}}      
+   function dv() {
+      // ENVIRONMENT - Horizontal flight efficiency - MM - CHART
+      var crt_hfe_MM = new google.visualization.ChartWrapper(
+         {
+            chartType: 'LineChart',
+            containerId: 'crt_hfe_MM',
+            dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1KgOIBF93axvXD6ENQYvEsWHqnA8eW_RvGV4EQlGava4/edit?usp=sharing&sheet=HFE_MM&range=A1:H25&tq=where%20A%3D1',
+            options: {
+               chartArea: { left: 50, top: 20, width: '55%', height: '60%' },
+                  title: 'Horiz. en-route flight eff. (monthly)',
+                  titleTextStyle: { color: 'grey', fontSize: 11 },
+                  legend: {
+                     position: 'right',
+                     textStyle: { color: 'grey', fontSize: 10 }
+                  },
+                  series: {
+                     0: {
+                        type: 'lines',
+                        color: '#5D7CBA',
+                        lineWidth: 1,
+                        pointSize: 3
+                     },
+                     1: {
+                        type: 'lines',
+                        color: '#C0504D',
+                        lineWidth: 1,
+                        pointSize: 3,
+                        pointShape: 'diamond'
+                     },
+                     2: {
+                        type: 'lines',
+                        color: '#5D7CBA',
+                        lineWidth: 2,
+                        pointSize: 3,
+                        lineDashStyle: [6, 3]
+                     },
+                     3: {
+                        type: 'lines',
+                        color:'#C0504D',
+                        lineWidth: 2,
+                        pointSize: 3,
+                        lineDashStyle: [6, 3],
+                        pointShape:'diamond'
+                     }
+                  },
+               vAxis: {
+                  title: 'inefficiency (%)',
+                  titleTextStyle: { color: 'grey', fontSize: 10 },
+                  minValue: 0.0,
+                  maxValue: 0.06,
+                  viewWindow: { min: 0.02, max: 0.05 },
+                  format: '0.0%'
                },
-      view: {'columns': [1,2,3,6,7] }
-   }); crt_hfe_MM.draw();   
+               hAxis: {
+                  title: 'Source: PRU Analysis',
+                  titleTextStyle: { color: 'grey', fontSize: 10 },
+                  type: 'string',
+                  slantedTextAngle: 90,
+                  textStyle: { fontSize: 11 }
+               }
+            },
+            view: { 'columns': [1, 2, 3, 6, 7] }
+         }
+      );
+      crt_hfe_MM.draw();
    }
-   google.setOnLoadCallback(dv)
+
+   google.setOnLoadCallback(dv);
 })();
