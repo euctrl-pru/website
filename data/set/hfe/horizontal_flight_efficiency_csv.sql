5b3fcd02287578e4fcec5d1754c1db122e8b4efc
Enrico Spinielli <espinielli@users.noreply.github.com>
2016-04-06 16:58:41 +0200

    new query following FABEC's request

--- data/set/hfe/horizontal_flight_efficiency_csv.sql
+++ data/set/hfe/horizontal_flight_efficiency_csv.sql
@@ -6,63 +6,85 @@ SET TRIMSPOOL ON;
 spool '&1';
 
 
-with inp1 as (select cast('31-mar-2016' as date) enddate, 10 bw_days from dual)
+with days as (select distinct trunc(entry_date +1, 'MON') -1 end_day from HFE_DAILY where entry_date >= '31-dec-2014')
 
-, inp2 as (select
-extract(year from enddate+1) incomplete_year
-, enddate - 364 - correctforleap(enddate) start_running
-, enddate end_running
-, bw_days / (365 + correctforleap(enddate)) cutoff from inp1)
+, inp
+              AS (SELECT DISTINCT
+                         mes_area
+,                        end_day
+,                        CASE model_type
+                            WHEN 'FTFM' THEN 'KEP'
+                            WHEN 'CPF' THEN 'KEA'
+                            ELSE 'ERR'
+                         END
+                            key_indicator
+,                        entry_date
+,                        flown_km
+,                        achieved_km
+,                        CASE
+                            WHEN DENSE_RANK ()
+                                 OVER (PARTITION BY mes_area, end_day, model_type
+                                       ORDER BY flown_km / achieved_km) <=
+                                    10
+                            THEN
+                               'EL'
+                            WHEN DENSE_RANK ()
+                                 OVER (PARTITION BY mes_area, end_day, model_type
+                                       ORDER BY flown_km / achieved_km) >
+                                    365 + correctforleap(end_day) - 10
+                            THEN
+                               'EH'
+                            ELSE
+                               'I'
+                         END
+                            cat
+                    FROM days join  HFE_DAILY on(
+                        entry_date > end_day - 365 - correctforleap(end_day)
+                         AND entry_date <= end_day)
+                         )
+
+,             inter
+              AS (SELECT DISTINCT
+                         mes_area
+,                        end_day
+,                        key_indicator
+,                        cat
+,                          SUM (flown_km)
+                              OVER (PARTITION BY mes_area, end_day, key_indicator)
+                         / SUM (achieved_km)
+                              OVER (PARTITION BY mes_area, end_day, key_indicator)
+                            hfe
+,                          SUM (
+                              flown_km)
+                           OVER (
+                              PARTITION BY mes_area, end_day, key_indicator, cat)
+                         / SUM (
+                              achieved_km)
+                           OVER (
+                              PARTITION BY mes_area, end_day, key_indicator, cat)
+                            hfe_cat
+,                        LISTAGG (entry_date, ', ')
+                         WITHIN GROUP (ORDER BY entry_date)
+                         OVER (PARTITION BY mes_area, end_day, key_indicator, cat)
+                            dates_cat
+                    FROM inp)
 , output as (
-SELECT
- extract(year from od.entry_date) YEAR
-,      EXTRACT (MONTH FROM od.entry_date) MONTH_NUM
-,      TO_CHAR (od.entry_date, 'MON') MONTH_MON
-,      od.entry_date
-,      ENTITY_NAME
-,      ENTITY_TYPE
-,      model_type AS TYPE_MODEL
-,      od.flown_km AS DIST_FLOWN_KM
-,      od.direct_km as DIST_DIRECT_KM
-,      od.achieved_km AS DIST_ACHIEVED_KM
-,      CASE
-          when entry_date < start_running or entry_date > end_running then 'O'
-          WHEN PERCENT_RANK ()
-               OVER (PARTITION BY mes_area, model_type, case when entry_date between start_running and end_running then 1 else 0 end
-                     ORDER BY od.flown_km / od.achieved_km) <= cutoff
-          THEN
-             'B' --  || ' (' || start_running || ' to ' || end_running || ')'
-          WHEN PERCENT_RANK ()
-               OVER (PARTITION BY mes_area, model_type, case when entry_date between start_running and end_running then 1 else 0 end
-                     ORDER BY od.flown_km / od.achieved_km) >= 1 - cutoff
-          THEN
-             'W' -- || ' (' || start_running || ' to ' || end_running || ')'
-          else
-             'I' -- || ' (' || start_running || ' to ' || end_running || ')'
-       END
-          T10_RUN_12M
-, end_running end_date
-,      CASE
-          --when extract (year from entry_date) >= incomplete_year then 'O'
-          WHEN PERCENT_RANK ()
-               OVER (PARTITION BY mes_area, model_type, extract (year from entry_date)
-                     ORDER BY od.flown_km / od.achieved_km) <= cutoff
-         THEN
-             'B'  -- || ' ' || extract (year from entry_date)
-          WHEN PERCENT_RANK ()
-               OVER (PARTITION BY mes_area, model_type, extract (year from entry_date)
-                     ORDER BY od.flown_km / od.achieved_km) >= 1 - cutoff
-          THEN
-             'W'  -- || ' ' || extract (year from entry_date)
-          else
-             'I'  -- || ' ' || extract (year from entry_date)
-       END
-          T10_YYYY
-  FROM inp2,
-       PRUTEST.HFE_DAILY od
-       join PRUTEST.DSH_BRIDGE_NAMES using(mes_area)
-  where entry_date <= end_running
-  ORDER by 4,5
+         SELECT entity_name
+         , entity_type
+,               end_day
+,               key_indicator
+,               ROUND ( (i.hfe - 1) * 100, 2) hfe
+,               ROUND ( (i.hfe_cat - 1) * 100, 2) corrected
+,               l.dates_cat excluded_low
+,               ROUND ( (l.hfe_cat - 1) * 100, 2) best_hfe
+,               h.dates_cat excluded_high
+,               ROUND ( (h.hfe_cat - 1) * 100, 2) worst_hfe
+
+           FROM inter i join PRUTEST.DSH_BRIDGE_NAMES using(mes_area)
+                JOIN inter l USING (mes_area, end_day, key_indicator)
+                JOIN inter h USING (mes_area, end_day, key_indicator)
+          WHERE l.cat = 'EL' AND h.cat = 'EH' AND i.cat = 'I'
+          ;
 )
 
 select * from output
