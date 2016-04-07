7b4f85ca8ea032dd5a6330b0b6a920ba756f8224
Enrico Spinielli <espinielli@users.noreply.github.com>
2016-04-07 10:40:11 +0200

    updated HFE SQL with direct/achieved distance rounding to 2 decimals only

--- data/set/hfe/horizontal_flight_efficiency_csv.sql
+++ data/set/hfe/horizontal_flight_efficiency_csv.sql
@@ -5,9 +5,8 @@ SET TRIMSPOOL ON;
 -- take name from argument 1
 spool '&1';
 
-
 WITH sd AS (SELECT CAST ('01-jan-2014' AS DATE) start_date FROM DUAL)
-  SELECT EXTRACT (YEAR FROM entry_date) YEAR
+SELECT EXTRACT (YEAR FROM entry_date) YEAR
 ,        EXTRACT (MONTH FROM entry_date) MONTH_NUM
 ,        TO_CHAR (entry_date, 'MON') MONTH_MON
 ,        entry_date
@@ -15,14 +14,14 @@ WITH sd AS (SELECT CAST ('01-jan-2014' AS DATE) start_date FROM DUAL)
 ,        ENTITY_TYPE
 ,        model_type AS TYPE_MODEL
 ,        flown_km AS DIST_FLOWN_KM
-,        direct_km AS DIST_DIRECT_KM
-,        achieved_km AS DIST_ACHIEVED_KM
-    FROM PRUTEST.HFE_DAILY JOIN PRUTEST.DSH_BRIDGE_NAMES USING (mes_area)
-   WHERE entry_date BETWEEN (SELECT start_date FROM sd)
-                        AND (SELECT MAX (TRUNC (entry_date + 1, 'MON') - 1)
-                                       end_day
-                               FROM sd, HFE_DAILY
-                              WHERE entry_date >= start_date)
+,        round(direct_km, 2) AS DIST_DIRECT_KM
+,        round(achieved_km, 2) AS DIST_ACHIEVED_KM
+FROM PRUTEST.HFE_DAILY JOIN PRUTEST.DSH_BRIDGE_NAMES USING (mes_area)
+WHERE entry_date BETWEEN (SELECT start_date FROM sd)
+AND (SELECT MAX (TRUNC (entry_date + 1, 'MON') - 1)
+end_day
+FROM sd, HFE_DAILY
+WHERE entry_date >= start_date)
 ORDER BY 4, 5;
 
 spool off;
