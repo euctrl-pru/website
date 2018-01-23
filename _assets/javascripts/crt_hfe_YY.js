d087c17583cfb764a8667501daa12f54b56bd0d0
HEGENDORFER Holger <holger.hegendorfer@eurocontrol.int>
2018-01-23 10:37:28 +0100

    fix flight efficiency

--- _assets/javascripts/crt_hfe_YY.js
+++ _assets/javascripts/crt_hfe_YY.js
@@ -30,11 +30,11 @@
                         }
                      },
                vAxis: {
-                  title: 'Inefficiency (%)',
+                  title: 'Efficiency (%)',
                   titleTextStyle: { color: 'grey', fontSize: 10 },
                   minValue: 0.0,
-                  maxValue: 0.06,
-                  viewWindow: { min: 0.02, max: 0.05 },
+                  maxValue: 0.1,
+                  viewWindow: { min: 0.95, max: 0.1 },
                   format: '0.0%'
                },
                hAxis: {
